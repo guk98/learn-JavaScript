1268e7de2424be95bf8d2f9e17436ed1cb39515d
guk98 <guk987@naver.com>
2025-11-25 10:59:24 +0900

    first commit

---
+++ basic.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+// 변수
+
+const milkCaramel = "빨간펜";
+let youtube = "후츠릿";
+youtube = "십오야";
+console.log("🚀 ~ youtube:", youtube);
+
+console.log("milk caramel", milkCaramel);
+
+const age = 28;
+console.log("🚀 ~ age:", age, typeof age);
+
+const isComing = true;
+console.log("🚀 ~ isComing:", isComing, typeof isComing);
+
+function sayHello() {
+  console.log("hello world");
+}
+
+sayHello();
+sayHello();
+sayHello();
